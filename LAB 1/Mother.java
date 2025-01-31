d73a17e0b459e1c511721ed34b17f6b5b6c7ab36
221B236 <221B236@juet.co.in>
2025-01-31 14:38:05 +0530

    adding LE2

---
+++ LAB 1/Mother.java
@@ -0,0 +1,10 @@
+
+public class Mother
+{
+    int x;
+    void show()
+    {
+        System.out.println("mother class is called");
+    }
+
+}
---
+++ LAB 1/child.java
@@ -0,0 +1,7 @@
+class child extends Mother
+{
+    void show()
+    {
+        System.out.println("child class is called "+x+" times");
+    }
+}
